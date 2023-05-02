61c6bf90220c14970ea07a1c0988c9593dd66c33
Kristina <kristina.skoro@yandex.ru>
2023-05-02 20:14:15 +0300

    Собственный лимит запросов

--- kittygram2plus/settings.py
+++ kittygram2plus/settings.py
@@ -132,9 +132,10 @@ REST_FRAMEWORK = {
     ],
 
     'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.UserRateThrottle',
         'rest_framework.throttling.ScopedRateThrottle',
     ],
-    
+
     'DEFAULT_THROTTLE_RATES': {
         'user': '10000/day', 
         'anon': '1000/day',  
